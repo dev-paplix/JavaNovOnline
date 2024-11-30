e1b4fc3488eccbdb5ed99d39a36a686b304451ba
devops.arman <paplix.dev@gmail.com>
2024-11-30 12:36:58 +0800

    loops

---
+++ InfitiniteWhileLoop.java
@@ -0,0 +1,13 @@
+public class InfitiniteWhileLoop {
+
+    public static void main(String[] args) {
+        int counter = 1;
+        while (counter <= 5) {
+            System.out.println("Counter " + counter);
+            counter--;
+            
+      
+        }
+    }
+    
+}
